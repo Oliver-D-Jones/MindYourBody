0f91a2b5f36c776ce94245f8d36e70021b65dbb5
OliverDJones <oliverjones.boise@gmail.com>
2020-08-11 15:49:59 -0600

    Capstone Template

---
+++ clientsrc/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+<style>
+
+</style>
