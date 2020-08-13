983f7585b9808a69c6176a69be79b12a672204a3
Landis Duffett <ldavduff@yahoo.com>
2020-08-13 15:00:03 -0600

    finishedendgame

--- clientsrc/src/components/endgame.vue
+++ clientsrc/src/components/endgame.vue
@@ -1,54 +1,70 @@
 <template>
   <div>
     <h1>End of Round</h1>
-    <h3 v-if="answer">You just earned {{points}} points!</h3>
+    <h3 v-if="answer">
+      You just earned {{points}} points!
+      <img src="../assets/coin.gif" style="width: 15rem;" />
+    </h3>
     <h3 v-else>No points this round.</h3>
+    <img src="../assets/frown.png" style="width: 15rem;" />
+    <h3>Play Again?</h3>
+    <div class="row m-4" style="justify-content: space-evenly">
+      <select name="category_id" v-model="subject" class="form-control" style="max-width: 35%">
+        <option value hidden>Subject</option>
+        <option>--- Select Subject ---</option>
+        <option value="any">Random</option>
+        <option value="9">general knowledge</option>
+        <option value="10">Books</option>
+        <option value="11">Film</option>
+        <option value="12">Music</option>
+        <option value="13">Musicals & Theatre</option>
+        <option value="14">Telvision</option>
+        <option value="15">Video Games</option>
+        <option value="16">Board Games</option>
+        <option value="17">Science & Nature</option>
+        <option value="18">Computers</option>
+        <option value="19">Mathematics</option>
+        <option value="20">Mythology</option>
+        <option value="21">Sports</option>
+        <option value="22">Geography</option>
+        <option value="23">History</option>
+        <option value="24">Politics</option>
+        <option value="25">Art</option>
+        <option value="26">Celebrities</option>
+        <option value="27">Animals</option>
+        <option value="28">Vehicles</option>
+        <option value="29">Entertainment</option>
+        <option value="30">Gadgets</option>
+        <option value="31">Japanese Anime & Manga</option>
+        <option value="32">Cartoon & Animations</option>
+      </select>
+      <select name="category_id" v-model="level" class="form-control" style="max-width: 35%">
+        <option value hidden>Level</option>
+        <option>--- Select Level ---</option>
+        <option value="easy">easy</option>
+        <option value="medium">medium</option>
+        <option value="hard">hard</option>
+      </select>
+    </div>
+    <div>
+      <button @click="startPlay()" class="btn btn-danger btn-lg border mb-3">START</button>
+    </div>
+    <h3>OR</h3>
+    <div>
+      <button @click="quit()" class="btn btn-danger btn-lg border mt-3">QUIT</button>
+    </div>
   </div>
 </template>
 
 <script>
+import router from "../router";
 export default {
-  name: "answer",
-  data: function () {
-    let answers = this.$store.state.trivia.incorrect_answers;
-    let correct_answer = this.$store.state.trivia.correct_answer;
-    swal(this.$store.state.trivia.question, {
-      className: "red-bg",
-      closeOnClickOutside: false,
-      buttons: {
-        zero: {
-          text: answers[0],
-          value: answers[0],
-        },
-        two: {
-          text: answers[1],
-          value: answers[1],
-        },
-        three: {
-          text: answers[2],
-          value: answers[2],
-        },
-        four: {
-          text: answers[3],
-          value: answers[3],
-        },
-      },
-    }).then((value) => {
-      if (value == correct_answer) {
-        swal(`${correct_answer}`, "Is Correct!", "success").then(() => {
-          swal.close();
-          this.$emit("init", true);
-        });
-      } else {
-        swal("Incorrect.", "Try Again.", "error").then(() => {
-          swal.close();
-          this.$emit("init", true);
-        });
-      }
-
-      this.show = false;
-    });
-    return {};
+  name: "endgame",
+  data() {
+    return {
+      subject: "",
+      level: "",
+    };
     //trigger next to display 'Next Round' Modal component
   },
   mounted() {
@@ -73,30 +89,29 @@ export default {
       return pts;
     },
   },
-  methods: {},
+  methods: {
+    startPlay() {
+      if (this.subject == "") {
+        this.subject = "any";
+      }
+      if (this.level == "") {
+        this.level = "any";
+      }
+      this.$store.dispatch("setSubject", this.subject);
+      this.$store.dispatch("setLevel", this.level);
+      console.log(this.subject);
+      console.log(this.level);
+      this.subject = "";
+      this.level = "";
+      router.push({ name: "game" });
+    },
+    quit() {
+      router.push({ name: "home" });
+    },
+  },
   components: {},
 };
 </script>
 
 
 <style scoped></style>
- swal({
-        title: "Question",
-        text: this.trivia.question,
-        className: "red-bg",
-        closeOnClickOutside: false,
-        buttons: {
-          zero: { text: "Start Exercise", value: true },
-          two: { text: "Quit Game", value: false },
-        },
-      }).then((value) => {
-        if (value) {
-          // display close and display excercize
-          this.excercizeCall = true;
-          this.answer = true;
-        } else {
-          this.$router.push({ name: "home" });
-        }
-      });
-      this.start = false;
-    },
--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
+import endgame from '../components/endgame.vue'
 import Game from '../views/Game.vue'
 import { authGuard } from "@bcwdev/auth0-vue"
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/endgame',
+      name: 'endgame',
+      component: endgame
     },
     {
       path: '/game',
